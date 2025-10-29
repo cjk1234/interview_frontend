3b63d46f7f96ba82e94c87c5c627a2168c46fe0b
cjk <11648006+chenjingkai123_0@user.noreply.gitee.com>
2025-10-29 20:48:48 +0800

    暂时前端与网页连接使用https，前后端连接用http

--- vue.config.js
+++ vue.config.js
@@ -5,15 +5,21 @@ module.exports = defineConfig({
   devServer: {
     port: 3000,
     host: '0.0.0.0',
+    https: {
+      key: require('fs').readFileSync('C:/Users/chenjingkai/server.key'),
+      cert: require('fs').readFileSync('C:/Users/chenjingkai/server.crt')
+    },
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
+        target: 'http://localhost:8443',
+        changeOrigin: true,
+        secure: false,
       },
       '/api/ws': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8443',
         ws: false,   // 保持 ws: true 不会影响 HTTP 降级方案的使用
-        changeOrigin: true
+        changeOrigin: true,
+        secure: false,
       }
     }
   }

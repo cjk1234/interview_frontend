0f9eab0d85cc6a199f8241b6334fde5ec900bfcc
cjk <11648006+chenjingkai123_0@user.noreply.gitee.com>
2026-01-14 22:47:40 +0800

    enable https

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs') // 引入 Node.js 文件系统模块
 
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 3000,
     host: '0.0.0.0',
-    // https: {
-    //   key: require('fs').readFileSync('C:/Users/chenjingkai/server.key'),
-    //   cert: require('fs').readFileSync('C:/Users/chenjingkai/server.crt')
-    // },
+    // 配置自定义 HTTPS 证书
+    server: {
+      type: 'https',
+      options: {
+        key: fs.readFileSync('./localhost+2-key.pem'), // 你的私钥文件路径
+        cert: fs.readFileSync('./localhost+2.pem'),     // 你的证书文件路径
+      },
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
@@ -17,7 +22,7 @@ module.exports = defineConfig({
       },
       '/api/ws': {
         target: 'http://localhost:8080',
-        ws: false,   // 保持 ws: true 不会影响 HTTP 降级方案的使用
+        ws: true,
         changeOrigin: true,
         secure: false,
       }
